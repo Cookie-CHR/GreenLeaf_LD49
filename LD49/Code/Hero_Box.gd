317ac77608850a40856b1b2dce0beb138589a0db
Cookie-CHR <pacchetto99@gmail.com>
2021-10-09 19:04:13 +0200

    ⚙️ AFTER JAM UPDATE - Fixed those collision shapes

--- LD49/Code/Hero_Box.gd
+++ LD49/Code/Hero_Box.gd
@@ -3,19 +3,37 @@ extends Sprite
 var box1 = preload("res://Images/Box_1.png")
 var box2 = preload("res://Images/Box_2.png")
 var box3 = preload("res://Images/Box_3.png")
-
+var path
 
 var velocity = Vector2()
 
 func get_input():
 	if Input.is_action_pressed("box_1"):
 		self.texture = box1
+		
+		path = str(self.get_path()).replace("Sprite3","")
+		get_node(path+"Polygon1").disabled = false
+		get_node(path+"Polygon2").disabled = true
+		get_node(path+"Polygon3").disabled = true
+		
 		get_parent().set_name("1")
 	if Input.is_action_pressed("box_2"):
 		self.texture = box2
+		
+		path = str(self.get_path()).replace("Sprite3","")
+		get_node(path+"Polygon1").disabled = true
+		get_node(path+"Polygon2").disabled = false
+		get_node(path+"Polygon3").disabled = true
+		
 		get_parent().set_name("2")
 	if Input.is_action_pressed("box_3"):
 		self.texture = box3
+		
+		path = str(self.get_path()).replace("Sprite3","")
+		get_node(path+"Polygon1").disabled = true
+		get_node(path+"Polygon2").disabled = true
+		get_node(path+"Polygon3").disabled = false
+		
 		get_parent().set_name("3")
 	
 func _physics_process(delta):
--- LD49/Code/Trash_sprite.gd
+++ LD49/Code/Trash_sprite.gd
@@ -4,6 +4,7 @@ var rng = RandomNumberGenerator.new()
 var t1 = preload("res://Images/Trash_1.png")
 var t2 = preload("res://Images/Trash_2.png")
 var t3 = preload("res://Images/Trash_3.png")
+var path
 
 func _ready():
 	rng.randomize()
@@ -11,12 +12,30 @@ func _ready():
 	if x == 1:
 		self.texture = t1
 		get_parent().set_name("1")
+		
+		path = str(self.get_path()).replace("Sprite","")
+		get_node(path+"Polygon1").disabled = false
+		get_node(path+"Polygon2").disabled = true
+		get_node(path+"Polygon3").disabled = true
+		
 	elif x == 2:
 		self.texture = t2
 		get_parent().set_name("2")
+		
+		path = str(self.get_path()).replace("Sprite","")
+		get_node(path+"Polygon1").disabled = true
+		get_node(path+"Polygon2").disabled = false
+		get_node(path+"Polygon3").disabled = true
+		
 	else:
 		self.texture = t3
 		get_parent().set_name("3")
+		
+		path = str(self.get_path()).replace("Sprite","")
+		get_node(path+"Polygon1").disabled = true
+		get_node(path+"Polygon2").disabled = true
+		get_node(path+"Polygon3").disabled = false
+		
 	self.set_scale(Vector2(0.5,0.5))
 	
 func _process(delta):
